a186708c60b26c60c3b1f80092279c132bf9e254
eswariah_bhun <eswariah_bhun@gap.com>
2015-06-19 10:46:15 +0530

    Modified logic to add number using varArgs

--- src/main/java/com/general/logics/AdditionLogics.java
+++ src/main/java/com/general/logics/AdditionLogics.java
@@ -2,13 +2,17 @@ package com.general.logics;
 
 public class AdditionLogics {
 
-	public int getAdditionOfTwoNumbers(int n1, int n2) {
-		return n1 + n2;
+	public int getAdditionNumbers(Integer...numbers) {
+		Integer result = 0;
+		for (Integer number : numbers) {
+			result += number;
+		}
+		return result;
 	}
 
-	public int getAdditionOfThreeNumbers(int n1, int n2, int n3) {
+	/*public int getAdditionOfThreeNumbers(int n1, int n2, int n3) {
 		return n1 + n2 + n3;
-	}
+	}*/
 	
 	public int getBonusAddition(int n1, int n2){
 		return n1 + n2 + 5;
--- src/test/java/com/general/logics/AdditionLogicsTest.java
+++ src/test/java/com/general/logics/AdditionLogicsTest.java
@@ -11,14 +11,14 @@ public class AdditionLogicsTest {
 	
 	@Test
 	public void getResult_AdditionOf2Numbers(){
-		int result = additionLogics.getAdditionOfTwoNumbers(5, 5);
+		int result = additionLogics.getAdditionNumbers(5, 5);
 		
 		assertThat(result, Is.is(10));
 	}
 	
 	@Test
 	public void getResult_AdditionOf3Numbers(){
-		int result = additionLogics.getAdditionOfThreeNumbers(5, 5, 5);
+		int result = additionLogics.getAdditionNumbers(5, 5, 5);
 		
 		assertThat(result, Is.is(15));
 	}
